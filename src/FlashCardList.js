8fcb2262eb43dc04760fd8fe66370d6358876f16
Sikandar <sikandarbutt@hotmail.co.uk>
2024-01-10 12:18:01 +0000

    added flash card list component

---
+++ src/FlashCardList.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+import React, { useState, useEffect, useCallback } from "react";
+import Flashcard from "./Flashcard";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import { AiOutlineForward } from "react-icons/ai";
+import "./flaschcardlist.css";
+import { FaHourglassHalf, FaStar } from "react-icons/fa";
+
+export default function FlashcardList({}) {
+  return (
+    <div></div>
+  );
+}
