d8cc9cb76c96dc51d431cb23f9b9f46626e721cb
Sikandar <sikandarbutt@hotmail.co.uk>
2024-01-10 11:50:54 +0000

    added flash card UI with radio buttons

--- src/Flashcard.js
+++ src/Flashcard.js
@@ -5,6 +5,39 @@ export default function Flashcard({ flashcard, onOptionSelect }) {
 
 
   return (
-    <div></div>
+    <div
+    className={`card ${flip ? "flip" : ""} ${
+      flashcard.userGenerated ? "user-generated" : ""
+    }`}
+    style={{ height: height }}
+    onClick={handleCardClick}
+  >
+    <Typography className="front " ref={frontEl}>
+      {flashcard.question}
+      {flashcard.options.length > 1 && (
+        <div className="flashcard-options mt-4">
+          {flashcard.options.map((option) => (
+            <div className="radio-option mb-2">
+              <FormControlLabel
+                control={
+                  <Radio
+                    id={option}
+                    name="flashcardOptions"
+                    value={option}
+                    checked={selectedOption === option}
+                    onChange={handleRadioChange}
+                    disabled={flip || optionDisabled}
+                    color="primary"
+                  />
+                }
+                label={option}
+                htmlFor={option}
+              />
+            </div>
+          ))}
+        </div>
+      )}
+    </Typography>
+    </div>
   );
 }
